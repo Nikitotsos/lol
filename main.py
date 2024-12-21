ed341809164962ee898c4f4d6b3b8a8db1c46de1
Nikitotsos <n.ie.vdovenko@gmail.com>
2024-12-21 14:39:02 +0200

    fg

--- main.py
+++ main.py
@@ -30,3 +30,53 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+# class Transport: 
+#     def init(self, brand, model, year): 
+#         self.brand = brand 
+#         self.model = model 
+#         self.year = year 
+ 
+#     def get_info(self): 
+#         return f"Brand: {self.brand}, Model: {self.model}, Year: {self.year}" 
+ 
+#     def honk(self): 
+#         return "Generic transport sound!" 
+ 
+ 
+# class Car(Transport): 
+#     def init(self, brand, model, year, fuel_type): 
+#         super().init(brand, model, year) 
+#         self.fuel_type = fuel_type 
+ 
+#     def get_info(self): 
+#         return f"{super().get_info()}, Fuel Type: {self.fuel_type}" 
+ 
+#     def honk(self): 
+#         return "Beep-beep!" 
+ 
+ 
+# class Bicycle(Transport): 
+#     def init(self, brand, model, year, gear_count): 
+#         super().init(brand, model, year) 
+#         self.gear_count = gear_count 
+ 
+#     def get_info(self): 
+#         return f"{super().get_info()}, Gear Count: {self.gear_count}" 
+ 
+#     def honk(self): 
+#         return "Ding-ding!" 
+ 
+ 
+# if name == "main": 
+  
+#     car = Car("Mercedes-AMG", " CLE Coupe", 2024, "Gasoline") 
+#     print(car.get_info())   
+#     print(car.honk())       
+ 
+#     bicycle = Bicycle("Giant", "Escape 3", 2022, 21) 
+#     print(bicycle.get_info())   
+#     print(bicycle.honk())
